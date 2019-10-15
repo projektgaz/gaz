cb528b8df80159a843edb62928d9a12647874c16
student <student@WIN_10_32>
2019-10-15 09:36:03 +0200

    głowna linia projektu

---
+++ TestSIOS/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace TestSIOS.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ TestSIOS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestSIOS.Global" Language="C#" %>

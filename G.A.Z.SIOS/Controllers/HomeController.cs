6f0d1f2e2d78bcd59572d7f9d046fca760956a07
Sebastian Trzeciak <sebastian.trzeciak@student.wat.edu.pl>
2019-10-22 22:51:27 +0200

    Model startowy

---
+++ G.A.Z.SIOS/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace G.A.Z.SIOS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Imprezy_studenckie()
+        {
+            ViewBag.Message = "Aktualne wydarzenia studenckie.";
+
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Dodaj_wydarzenie()
+        {
+            ViewBag.Message = "Aktualne wydarzenia studenckie.";
+
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Opinie_i_raporty()
+        {
+            ViewBag.Message = "Aktualne wydarzenia studenckie.";
+
+            return View();
+        }
+    }
+}
---
+++ G.A.Z.SIOS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(G.A.Z.SIOS.Startup))]
+namespace G.A.Z.SIOS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

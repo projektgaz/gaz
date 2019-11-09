baf641f1e4737ca3517ed43aa28761376215a050
Sebastian Trzeciak <sebastian.trzeciak@student.wat.edu.pl>
2019-11-09 21:41:44 +0100

    bootstrap 3.4.1 slate https://bootswatch.com/3/slate/

--- G.A.Z.SIOS/App_Start/BundleConfig.cs
+++ G.A.Z.SIOS/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace G.A.Z.SIOS
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap_slate.css",
                       "~/Content/site.css"));
         }
     }

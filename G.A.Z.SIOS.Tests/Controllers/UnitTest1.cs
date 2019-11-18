7663b2b339eb0a42f1b268d41d450d159c0b17d7
Jędrzej Makowski <jedrzej.makowski@student.wat.edu.pl>
2019-11-18 20:00:39 +0100

    zmiana

---
+++ G.A.Z.SIOS.Tests/Controllers/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace G.A.Z.SIOS.Tests.Controllers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
